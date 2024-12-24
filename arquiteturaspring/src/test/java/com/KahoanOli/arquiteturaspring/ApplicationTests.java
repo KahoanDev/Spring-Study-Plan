233f07593ca9abd979f834cbe21945bf993e7519
KahoanOliveira <kahoan9@gmail.com>
2024-12-24 02:11:53 -0300

    :construction: Curso de Spring Iniciando o conteúdo introduzindo conceitos de REST e criação de controllers

---
+++ arquiteturaspring/src/test/java/com/KahoanOli/arquiteturaspring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.KahoanOli.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
